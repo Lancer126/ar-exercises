2cce069d0fae19fbda2ecb895b7fcda6dcaed838
Lancer126 <rthcontact@gmail.com>
2019-07-09 22:37:30 +0000

    Doing number 7

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,9 @@
 class Employee < ActiveRecord::Base
+    belongs_to :store
+    validates: :first_name, presence: true, :last_name, presence: true,
+    :hourly_rate, numericality: { only_integer: true }, if: between? 
 
+    def between?
+        200 > hourly_rate > 40
+    end
 end
